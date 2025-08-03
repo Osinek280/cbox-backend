67855d91f119cfbac04d66bcba3a9fa07e53943f
Szymon <szymon.osinski.2009@gmail.com>
2025-08-03 17:10:53 +0200

    rename CboxApplication

--- src/main/java/com/cbox/cbox/CboxApplication.java
+++ src/main/java/com/cbox/cbox/CboxApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EdumateApplication {
+public class CboxApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(EdumateApplication.class, args);
+		SpringApplication.run(CboxApplication.class, args);
 	}
 }
