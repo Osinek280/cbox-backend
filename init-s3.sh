c7c1415e292d2af4120f1d952d4a8cf1314be853
Szymon <szymon.osinski.2009@gmail.com>
2025-08-29 12:09:24 +0200

    fix(localstack): cors

--- init-s3.sh
+++ init-s3.sh
@@ -15,3 +15,17 @@ echo "Synchronizuję lokalny folder $LOCAL_FOLDER z bucketem $BUCKET..."
 awslocal s3 sync $LOCAL_FOLDER s3://$BUCKET/ --delete
 
 echo "Sync zakończony!"
+
+# Konfiguracja CORS przy użyciu awslocal
+echo "Ustawiam CORS na bucket $BUCKET..."
+awslocal s3api put-bucket-cors --bucket $BUCKET --cors-configuration '{
+  "CORSRules": [
+    {
+      "AllowedHeaders": ["*"],
+      "AllowedMethods": ["GET"],
+      "AllowedOrigins": ["*"]
+    }
+  ]
+}'
+
+echo "CORS ustawione!"
